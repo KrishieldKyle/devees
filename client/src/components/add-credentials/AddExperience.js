40f82fd05853169c61e65bfe4e0c95478aa19ae7
krishield kyle santoyo <krishieldkyle@gmail.com>
2018-12-01 23:41:35 +0800

    Add Experience Functionality

--- client/src/components/add-credentials/AddExperience.js
+++ client/src/components/add-credentials/AddExperience.js
@@ -4,6 +4,7 @@ import TextFieldGroup from '../common/TextFieldGroup';
 import TextAreaFieldGroup from '../common/TextAreaFieldGroup';
 import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
+import { addExperience } from '../../actions/profileActions';
 
 class AddExperience extends Component {
     constructor(props) {
@@ -22,6 +23,14 @@ class AddExperience extends Component {
         this.onChange = this.onChange.bind(this);
         this.onSubmit = this.onSubmit.bind(this);
         this.onCheck = this.onCheck.bind(this);
+    }
+
+    componentWillReceiveProps(nextProps) {
+        if (nextProps.errors) {
+            this.setState({
+                errors: nextProps.errors
+            })
+        }
     }
 
 
@@ -140,6 +149,7 @@ class AddExperience extends Component {
 }
 
 AddExperience.propTypes = {
+    addExperience: PropTypes.func.isRequired,
     profile: PropTypes.object.isRequired,
     errors: PropTypes.object.isRequired
 }
@@ -149,4 +159,4 @@ const mapStateToProops = (state) => ({
     errors: state.errors
 })
 
-export default connect(mapStateToProops)(withRouter(AddExperience));
+export default connect(mapStateToProops, { addExperience })(withRouter(AddExperience));
