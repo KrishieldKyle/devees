73cc338b8777ab827ac48a757f26cec24f31f73b
krishield kyle santoyo <krishieldkyle@gmail.com>
2018-12-02 22:15:20 +0800

    Prepare for deployment

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const passport = require('passport');
+const path = require('path')
 const moment = require("moment-timezone");
 const app = express();
 
@@ -39,6 +40,17 @@ require('./config/passport.js')(passport);
 app.use('/api/users', users);
 app.use('/api/profile', profile);
 app.use('/api/posts', posts);
+
+// Server static assests if in production
+if (process.env.NODE_ENV === 'production') {
+    // Set static folder
+    app.use(express.static('client/build'));
+
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    });
+
+}
 
 app.listen(port, () => {
     console.log(`Listening to port ${port}`)
